66bc4490cd07db38ae61d537ce9d28969e06f51a
Spencer Murray <spencer.murray@sentry.io>
2024-07-26 14:37:44 -0400

    Initial Commit

---
+++ test_main.py
@@ -0,0 +1,10 @@
+from main import add, subtract, multiply
+
+def test_add():
+    assert add(1, 2) == 3
+
+def test_subtract():
+    assert subtract(2, 1) == 1
+
+def test_multiply():
+    assert multiply(2, 3) == 6
